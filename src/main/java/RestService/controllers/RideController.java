ab4787fefb74ab47eeb90df8ffa67cf029d210be
Guy Shapira <Guy.shapira@campus.technion.ac.il>
2021-01-04 22:42:24 +0200

    proto completed (for now) grpc start

--- src/main/java/RestService/controllers/RideController.java
+++ src/main/java/RestService/controllers/RideController.java
@@ -1,11 +1,16 @@
 package RestService.controllers;
 
+import com.google.protobuf.TextFormat;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import io.grpc.StatusRuntimeException;
 import TaxiRide.*;
 import RestService.repository.*;
+import org.springframework.web.server.ResponseStatusException;
+import protos.TaxiRideProto;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -18,8 +23,38 @@ public class RideController {
     }
 
     @PostMapping("/rides")
-    void post_ride(@RequestBody Ride new_ride){
+    void post_ride(@RequestBody Ride new_ride) {
         Ride newRide = repository.save(new_ride);
+        LocalDate ride_date = new_ride.getDate();
+        City src_city = new_ride.getStart_location();
+        City end_city = new_ride.getEnd_location();
+
+        TaxiRideProto.RideRequest ride = TaxiRideProto.RideRequest
+                .newBuilder()
+                .setId(new_ride.getId())
+                .setFirstName(new_ride.getFirst_name())
+                .setLastName(new_ride.getLast_name())
+                .setDate(TaxiRideProto.Date.
+                        newBuilder()
+                        .setDay(ride_date.getDayOfMonth())
+                        .setMonth(ride_date.getMonthValue())
+                        .setYear(ride_date.getYear()))
+                .setEndLocation(TaxiRideProto.City
+                        .newBuilder()
+                        .setCityId(end_city.getCity_id())
+                        .setCityName(end_city.getCity_name())
+                        .setX(end_city.getX())
+                        .setY(end_city.getY())
+                )
+                .setStartLocation(TaxiRideProto.City
+                        .newBuilder()
+                        .setCityId(src_city.getCity_id())
+                        .setCityName(src_city.getCity_name())
+                        .setX(src_city.getX())
+                        .setY(src_city.getY())
+                ).build();
+
+        // user grpc to send ride to all servers
     }
 
     @GetMapping("/rides")
--- src/main/java/RestService/controllers/UserController.java
+++ src/main/java/RestService/controllers/UserController.java
@@ -1,13 +1,16 @@
 package RestService.controllers;
 
 import RestService.repository.UserRepository;
+import TaxiRide.City;
 import TaxiRide.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import protos.TaxiRideProto;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -21,7 +24,24 @@ public class UserController {
 
     @PostMapping("/users")
     void post_ride(@RequestBody User new_user){
-        User newRide = repository.save(new_user);
+        User newUser = repository.save(new_user);
+        City user_city = new_user.getLocation();
+
+        TaxiRideProto.UserRequest user = TaxiRideProto.UserRequest
+                .newBuilder()
+                .setId(new_user.getId())
+                .setFirstName(new_user.getFirst_name())
+                .setLastName(new_user.getLast_name())
+                .setLocation(TaxiRideProto.City
+                        .newBuilder()
+                        .setCityId(user_city.getCity_id())
+                        .setCityName(user_city.getCity_name())
+                        .setX(user_city.getX())
+                        .setY(user_city.getY())
+                ).build()
+                ;
+
+
     }
 
     @GetMapping("/users")
